4a8845c83078b4f139a2d6ec90ebdecc6cbe24e6
Ajay Javiya <ajaviya@odoogap.com>
2023-10-26 16:10:39 +0530

    [FIX] change product variant on sale order line does not update bom lines related to specific variant attributes

--- mrp_mto_draft/models/mrp_production.py
+++ mrp_mto_draft/models/mrp_production.py
@@ -14,18 +14,14 @@ class MrpProduction(models.Model):
                 is_from_so_configuration_product = True
             mo.is_from_so_configuration_product = is_from_so_configuration_product
 
-    def _update_bom_id(self):
-        self.product_uom_id = self.bom_id and self.bom_id.product_uom_id.id or self.product_id.uom_id.id
-        for move in self.move_raw_ids.filtered(lambda m: m.bom_line_id):
-            move.product_uom_qty = move.bom_line_id.product_qty * self.product_qty
-        self.picking_type_id = self.bom_id.picking_type_id or self.picking_type_id
-
     def _update_mo_from_sale(self, vals):
         if 'product_qty' in vals and not vals.get('product_qty', 0):
             self.action_cancel()
         else:
             self.write(vals)
+            self._onchange_product_id()
             self._onchange_product_qty()
             self._onchange_move_finished_product()
-            self._update_bom_id()
+            self.move_raw_ids = [(5,)]
+            self._onchange_move_raw()
             self._create_workorder()
